6c2edf323a9be93fadf68ae0c9d03507e92ee6fb
Peter Szarvas <contact@peterszarvas.hu>
2025-11-19 21:27:42 +0100

    init: ported to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://szarvaspongrac.hu",
+  build: {
+    assets: "assets",
+  },
+  compressHTML: false,
+  vite: {
+    build: {
+      minify: false,
+    },
+  },
+});
